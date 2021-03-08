8eda3a1872f5f0176b8ec2a8aae276417e03db89
ORuan <ruanpablomp@gmail.com>
2021-03-08 10:58:12 -0300

    deploy v2

--- core/settings.py
+++ core/settings.py
@@ -106,6 +106,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-
-if not DEBUG:
-    django_heroku.settings(locals())
+django_heroku.settings(locals())
