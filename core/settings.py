c9c2b614701fb52b3d3802a00a93685f5ded8400
ORuan <ruanpablomp@gmail.com>
2021-03-08 11:03:19 -0300

    deploy v3

--- core/settings.py
+++ core/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # CRITIC CREDENCIALS
 SECRET_KEY = os.environ.get('SECRET_KEY')
 #DEBUG = os.environ.get('DEBUG') == 'True'
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
